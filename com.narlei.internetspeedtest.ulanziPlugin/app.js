ae3b54938301474edc65539f1ba3fed99b220883
Narlei Moreira <narlei.guitar@gmail.com>
2026-07-15 07:42:13 -0300

    Initial Ulanzi plugin scaffold

---
+++ com.narlei.internetspeedtest.ulanziPlugin/app.js
@@ -0,0 +1,8 @@
+// Skeleton — edit to build your plugin logic
+$UD.on("connected", (evt) => {
+    console.log("Connected:", evt);
+});
+
+$UD.on("keyUp", (evt) => {
+    console.log("Key pressed:", evt);
+});
